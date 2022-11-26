444b47ad6bf7c70c3874638518ec3421b07e869c
SaFaUU <thulk283@gmail.com>
2022-11-26 17:26:24 +0600

    Fixed API for All Buyer and Seller

--- index.js
+++ index.js
@@ -17,12 +17,49 @@ async function run() {
         const usersCollection = client.db('cellflip').collection('users');
         app.post('/user', async (req, res) => {
             const user = req.body;
-            const result = await usersCollection.insertOne(user)
-            res.send(result)
+            const query = {
+                email: user.email
+            }
+            const dbUser = await usersCollection.findOne(query);
+            console.log(dbUser);
+            if (dbUser?.email) {
+                res.send({ message: 'User already exists' })
+            }
+            else {
+                const result = await usersCollection.insertOne(user)
+                res.send(result)
+            }
         })
-        app.get('/user', async (req, res) => {
+
+        app.get('/admin/:email', async (req, res) => {
+            const user_email = req.params.email;
+            const query = {
+                email: user_email
+            }
+            const user = await usersCollection.findOne(query);
+            res.send(user)
+        })
+        app.get('/user/:email', async (req, res) => {
+            const user_email = req.params.email;
+            const query = {
+                email: user_email
+            }
+            const user = await usersCollection.findOne(query);
+            res.send(user)
+        })
+
+        app.get('/users', async (req, res) => {
             const query = {
 
+            }
+            const result = await usersCollection.find(query).toArray()
+            res.send(result)
+        })
+
+        app.get('/user', async (req, res) => {
+            const role = req.query.role;
+            const query = {
+                role: role
             }
             const result = await usersCollection.find(query).toArray()
             res.send(result)
